63c9d7c1679fe3ea73ae20eae5b129e47b399d8b
jdduarte <jdduarte@gmail.com>
2012-08-29 23:10:34 +0100

    Added the Ranking model

---
+++ js/ranking.js
@@ -0,0 +1,12 @@
+//      Zombie Dice client. Made by Igor Soarez and João Duarte for lxjs 2012
+
+//      This is the Ranking model.
+//      It represents the game global player ranking.
+
+var Ranking = Backbone.Model.extend({
+    urlRoot: '/ranking',
+
+    initialize: function(){
+        
+    }
+});
